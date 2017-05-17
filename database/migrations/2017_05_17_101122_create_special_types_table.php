c235900b3c28529584f6e1269d1a84108012a4d5
Thanapong Prathumchat <thanapong.pr.56@ubu.ac.th>
2017-05-17 10:12:50 +0700

    Add special_type table migration

---
+++ database/migrations/2017_05_17_101122_create_special_types_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpecialTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('special_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('type_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('special_types');
+    }
+}
