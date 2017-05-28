d355dbef6fc4b5a94c5cf3aed5471531d28c51b5
Thanapong Prathumchat <thanapong.pr.56@ubu.ac.th>
2017-05-28 14:13:39 +0700

    Add eager loading constraint

--- app/Web/EventResult.php
+++ app/Web/EventResult.php
@@ -3,6 +3,7 @@
 namespace Atom26\Web;
 
 use Atom26\Accounts\User;
+use Atom26\Accounts\University;
 use Illuminate\Database\Eloquent\Model;
 
 class EventResult extends Model
@@ -13,6 +14,13 @@ class EventResult extends Model
      * @var array
      */
     protected $guarded = ['id'];
+
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var array
+     */
+    protected $with = ['athlete', 'sets', 'university'];
 
     /**
      * Get the athlete from this result.
@@ -32,5 +40,15 @@ class EventResult extends Model
     public function sets()
     {
         return $this->hasMany(EventSet::class);
+    }
+
+    /**
+     * Get the university info.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function university()
+    {
+        return $this->belongsTo(University::class);
     }
 }
