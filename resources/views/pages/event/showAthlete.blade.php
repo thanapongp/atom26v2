d168cb077e13fa509fc9f550862e7cd8acd38fc6
Thanapong Prathumchat <thanapong.pr.56@ubu.ac.th>
2017-05-29 13:34:08 +0700

    Add days tab to result views

--- resources/views/pages/event/showAthlete.blade.php
+++ resources/views/pages/event/showAthlete.blade.php
@@ -13,53 +13,66 @@
     @endif
 
     {{Date::setLocale('th')}}
-
+    
+    <ul class="nav nav-pills nav-fill mt-3" role="tablist">
+        @foreach($events as $day => $_)
+        <li class="nav-item">
+            <a class="nav-link{{$loop->first ? ' active' : ''}}" 
+            data-toggle="tab" href="#day{{$day}}" role="tab">
+            {{Date::parse($day)->format('j F Y')}}</a>
+        </li>
+        @endforeach
+    </ul>
+    
+    <div class="tab-content mt-5">
     @foreach($events as $date => $events)
-    <h5 class="mt-5">{{Date::parse($date)->format('j F Y')}}</h5>
-    <hr>
+    <div class="tab-pane{{$loop->first ? ' active' : ''}}" id="day{{$date}}"
+    role="tabpanel">
     
-    
-    <div id="date{{$date}}" role="tablist" aria-multiselectable="true">
+        <div id="date{{$date}}" role="tablist" aria-multiselectable="true">
 
-        @foreach($events as $event)
-        <div class="card">
-            <div class="card-header" role="tab" id="heading{{$event->id}}">
-                <a href="#collapse{{$event->id}}" data-toggle="collapse"
-                data-parent="date{{$date}}" aria-controls="collapse{{$event->id}}">
-                    ({{Date::parse($event->date)->format('H:i')}}) {{$event->name}}
-                </a>
-            </div>
+            @foreach($events as $event)
+            <div class="card">
+                <div class="card-header" role="tab" id="heading{{$event->id}}">
+                    <a href="#collapse{{$event->id}}" data-toggle="collapse"
+                    data-parent="date{{$date}}" aria-controls="collapse{{$event->id}}">
+                        ({{Date::parse($event->date)->format('H:i')}}) {{$event->name}}
+                    </a>
+                </div>
 
-            <div id="collapse{{$event->id}}" class="collapse p-4" role="tabpanel"
-            aria-labelledby="heading{{$event->id}}">
-                <table class="table table-stripped">
-                    <thead>
-                        <th>ลำดับที่</th>
-                        <th>{{$event->results[0]->athlete ? 'ชื่อนักกีฬา' : 'ชื่อสถาบัน'}}</th>
-                        <th>เวลา</th>
-                    </thead>
-                    <tbody>
-                        @foreach($event->results as $result)
-                        <tr>
-                        <td>{{$result->order}}</td>
-                        <td>
-                            <img src="/img/logo-uni/logo{{$result->university->code}}.png">
-                            {{$result->athlete ? "({$result->university->code}) " . $result->athlete->fullname() 
-                            : $result->university->name}}
-                        </td>
-                        <td>
-                            {{$result->time}}
-                        </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <div id="collapse{{$event->id}}" class="collapse p-4" role="tabpanel"
+                aria-labelledby="heading{{$event->id}}">
+                    <table class="table table-stripped">
+                        <thead>
+                            <th>ลำดับที่</th>
+                            <th>{{$event->results[0]->athlete ? 'ชื่อนักกีฬา' : 'ชื่อสถาบัน'}}</th>
+                            <th>เวลา</th>
+                        </thead>
+                        <tbody>
+                            @foreach($event->results as $result)
+                            <tr>
+                            <td>{{$result->order}}</td>
+                            <td>
+                                <img src="/img/logo-uni/logo{{$result->university->code}}.png">
+                                {{$result->athlete ? "({$result->university->code}) " . $result->athlete->fullname() 
+                                : $result->university->name}}
+                            </td>
+                            <td>
+                                {{$result->time}}
+                            </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
+             @endforeach
+
         </div>
-         @endforeach
 
     </div>
     @endforeach
+    </div>
 
 </div>
 @endsection
--- resources/views/pages/event/showTeamBased.blade.php
+++ resources/views/pages/event/showTeamBased.blade.php
@@ -14,59 +14,72 @@
 
     {{Date::setLocale('th')}}
 
+    <ul class="nav nav-pills nav-fill mt-3" role="tablist">
+        @foreach($events as $day => $_)
+        <li class="nav-item">
+            <a class="nav-link{{$loop->first ? ' active' : ''}}" 
+            data-toggle="tab" href="#day{{$day}}" role="tab">
+            {{Date::parse($day)->format('j F Y')}}</a>
+        </li>
+        @endforeach
+    </ul>
+    
+    <div class="tab-content mt-5">
     @foreach($events as $date => $events)
-    <h5 class="mt-5">{{Date::parse($date)->format('j F Y')}}</h5>
-    <hr>
-    
-    
-    <div id="date{{$date}}" role="tablist" aria-multiselectable="true">
+    <div class="tab-pane{{$loop->first ? ' active' : ''}}" id="day{{$date}}"
+    role="tabpanel">
 
-        @foreach($events as $event)
-        <div class="card mt-3">
-            <div class="card-header" role="tab" id="heading{{$event->id}}">
-                <a href="#collapse{{$event->id}}" data-toggle="collapse"
-                data-parent="#date{{$date}}" aria-controls="collapse{{$event->id}}">
-                    ({{Date::parse($event->date)->format('H:i')}}) {{$event->name}} 
-                    ({{$event->results[0]->university->code}} vs. {{$event->results[1]->university->code}})
-                </a>
-            </div>
+        <div id="date{{$date}}" role="tablist" aria-multiselectable="true">
 
-            <div id="collapse{{$event->id}}" class="collapse p-4" role="tabpanel"
-            aria-labelledby="heading{{$event->id}}">
-                <table class="table table-stripped">
-                    <thead>
-                        <th>{{$event->results[0]->athlete ? 'ชื่อนักกีฬา' : 'ชื่อสถาบัน'}}</th>
-                        <th>คะแนน</th>
-                    </thead>
-                    <tbody>
-                        @foreach($event->results as $result)
-                        <tr class="{{$result->is_winner ? 'table-warning' : ''}}">
-                        <td>
-                            <img src="/img/logo-uni/logo{{$result->university->code}}.png">
-                            {{$result->athlete ? "({$result->university->code}) " . $result->athlete->fullname() 
-                            : $result->university->name}}
-                        </td>
-                        <td>
-                            @if(is_null($result->score) && $result->is_winner)
-                            ชนะ
-                            @elseif(is_null($result->score) && ! $result->is_winner)
-                            แพ้
-                            @elseif($result->score == 'W')
-                            สละสิทธิ์
-                            @else
-                            {{$result->score}}
-                            @endif                           
-                        </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+            @foreach($events as $event)
+            <div class="card mt-3">
+                <div class="card-header" role="tab" id="heading{{$event->id}}">
+                    <a href="#collapse{{$event->id}}" data-toggle="collapse"
+                    data-parent="#date{{$date}}" aria-controls="collapse{{$event->id}}">
+                        ({{Date::parse($event->date)->format('H:i')}}) {{$event->name}} 
+                        ({{$event->results[0]->university->code}} vs. {{$event->results[1]->university->code}})
+                    </a>
+                </div>
+
+                <div id="collapse{{$event->id}}" class="collapse p-4" role="tabpanel"
+                aria-labelledby="heading{{$event->id}}">
+                    <table class="table table-stripped">
+                        <thead>
+                            <th>{{$event->results[0]->athlete ? 'ชื่อนักกีฬา' : 'ชื่อสถาบัน'}}</th>
+                            <th>คะแนน</th>
+                        </thead>
+                        <tbody>
+                            @foreach($event->results as $result)
+                            <tr class="{{$result->is_winner ? 'table-warning' : ''}}">
+                            <td>
+                                <img src="/img/logo-uni/logo{{$result->university->code}}.png">
+                                {{$result->athlete ? "({$result->university->code}) " . $result->athlete->fullname() 
+                                : $result->university->name}}
+                            </td>
+                            <td>
+                                @if(is_null($result->score) && $result->is_winner)
+                                ชนะ
+                                @elseif(is_null($result->score) && ! $result->is_winner)
+                                แพ้
+                                @elseif($result->score == 'W')
+                                สละสิทธิ์
+                                @else
+                                {{$result->score}}
+                                @endif                           
+                            </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
+             @endforeach
+
         </div>
-         @endforeach
 
     </div>
     @endforeach
+    </div>
 
 </div>
 @endsection
